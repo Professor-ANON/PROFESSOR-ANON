7704b8e9f5ddf89cd75934c93d896af4845e653d
Professor-ANON <hbarzani855@gmail.com>
2024-06-13 04:09:23 +0300

    Arrangement Termux
    Arrangement Termux 
    password tools: professorAnon

---
+++ PROFESSOR_ANON.py
@@ -0,0 +1,137 @@
+
+#CREATE BY [PROFESSOR-ANON]
+#TELEGRAM  [https://t.me/ANON_CRACKER1]
+
+import os
+import time
+from getpass import getpass
+num1=(getpass("\033[3m [PROFESSOR-ANON]>Password Bnusa:-  "))
+if num1 == "professorAnon":
+  print("Welcome")
+  os.system(" pkg install nudoku ; termux-setup-storage ; pkg install termux-contact-list ; pkg install neofeth ;pkg install espeak ; clear ;espeak 'Welcome to profssor ANON' ")
+else:
+  print("halayak haya!")
+  exit()
+  
+logo="""\033[0;31m
+_____             __
+|  _ \ _ __ ___  / _| ___  ___ ___  ___  _ __
+| |_) | '__/ _ \| |_ / _ \/ __/ __|/ _ \| '__|
+|  __/| | | (_) |  _|  __/\__ \__ \ (_) | |
+|_|   |_|  \___/|_|  \___||___/___/\___/|_|
+"""
+print(logo)
+nvesen="""\033[0;31m ____________________________________________
+
+CREATE BY [PROFESSOR-ANON]
+TELEGRAM  [https://t.me/ANON_CRACKER1]
+Bxer bey bo toole [PROFESSOR-ANON]
+____________________________________________
+"""
+print(nvesen)
+
+nvesen1="""\033[0;31m
+____________________________________________
+
+CREATE BY [PROFESSOR-ANON]
+
+____________________________________________
+
+[KURDISH]
+[1] bka bo rekxstn
+[2] bka bo logo 
+
+
+[0] sabarat ba mn
+[00] bka bo darchon-exit
+____________________________________________
+
+CREATE BY [PROFESSOR-ANON]
+
+____________________________________________
+
+[ENGLISH]
+[1] Arrangement termux command
+[2] Creating a Logo by Writing 
+
+
+[0] About Me
+[00] Exit
+
+____________________________________________
+
+"""
+print(nvesen1)
+num2=(input("[PROFESSOR-ANON]>Enter:- "))
+def anon():
+  os.system('\033[0;32m pkg update -y  pkg remove  science-repo -y  ; pkg remove  game-repo -y ; apt update; apt upgrade -y; apt install figlet -y ; figlet WA DAST PE AKAM ; pkg install sl -y ; apt install cmatrix -y ; pkg install coreutils -y ; clear ; figlet ANON ; apt install fish -y ; clear ; figlet PROFESSOR ; apt install toilet -y ; apt install w3m -y ; pkg install curl -y ; clear ; figlet PROFESSOR ; pkg install python -y ; pkg install python2 -y ; pkg install php -y ; clear ; figlet ANON ; pkg install clang -y ; apt install mechanize -y ; pkg install nano -y ; pkg install bash -y ; clear ; figlet sabrt habe ; pkg install ruby -y ; pip install --upgrade pip ; pip install requests ; pkg install python -y ; pkg install python2 -y ; pip install bs4 ; pkg install bash -y ; pkg update ; pkg upgrade-y ; pkg install nmaq -y ; apt install golang -y ; apt install host -y ; clear ; apt install nano -y ; apt install havij -y ; apt install hydra -y ; apt install wireshark -y ; apt install cmatrix -y ; pkg install wget -y ; pkg install cowsay -y ; figlet ANON ; pkg install toilet -y ; pkg install help -y ; pkg install lolcat -y ; pkg install curl -y ; pkg install wgetrc -t ; pkg install uzip -y ; pkg install openssh -y ; clear ; pkg install net-tools -y ; pkg install tor -y ; pkg install unrar -y ; pkg install clang -y ; gem install lolcat ; pkg install w3m -y ; pip2 install wget ; pkg install git -y ; figlet PROFESSOR ; pkg update ; pkg upgrade -y ; apt update ; apt upgrade -y ; clear ; figlet kamek mawa pkg install python2 pkg install git ; termux-setup-storage termux-tts-speak welcome to professorAnon ; apt update; apt upgrade -y; apt install figlet -y ; figlet ANON ; pkg install sl -y ; apt install cmatrix -y ; pkg install coreutils -y ; pkg install x11-repo ; pkg install root-repo ; clear ; figlet Bardawam ba ; apt install fish -y ; clear ; termux-tts-speak El professor ANON ;figlet ANON ; apt install toilet -y ; apt install w3m -y ; pkg install curl -y ; clear ; figlet ANONYMOUS ; pkg install python -y ; pkg install python2 -y ; pkg install php -y ; clear ; figlet ANON ; pkg install clang -y ;clear;figlet ANON apt install mechanize -y ; pkg install nano -y ; pkg install bash -y ; clear ; figlet sabrt habe ; pkg install ruby -y ; pip install --upgrade pip ; pip install requests ; pkg install python -y ; pkg install python2 -y ;clear; figlet ANON; pip install bs4 ; pkg install bash -y ; pkg update ; pkg update -y ; pkg upgrade-y ; pkg install nmaq -y ; apt install golang -y ; apt install host -y ; clear ; apt install nano -y ; apt install havij -y ; apt install hydra -y ; apt install wireshark -y ; apt install cmatrix -y ; pkg install wget -y ; pkg install cowsay -y ; figlet ANON ; pkg install toilet -y ; pkg install help -y ; pkg install lolcat -y ; pkg install curl -y ; pkg install wgetrc -t ; pkg install uzip -y ; pkg install openssh -y ; clear ; pkg install net-tools -y ; pkg install tor -y ; clear ; \033[0;31m figlet Sabrt habe;pkg install unrar -y ; pkg install clang -y ; gem install lolcat ; pkg install w3m -y ; pip2 install wget ; pkg install git -y ;\033[0;31m clear;figlet kamek mawa ; pkg update ; pkg upgrade -y ; apt update ; apt upgrade -y ; clear ;termux-tts-speak PROFESSOR ANON figlet ANON pkg install python2 pkg install git;pkg install python2;pkg install git; pip install bs4 ; pip install rich; pkg install rclone ;pip install requests ; pip install mechanize ; clear; termux-tts-speak El professor Anon ; figlet Tawa bo PROFESSOR-ANON')
+  exit()
+def anon2():
+  N29=(input("[PROFESSOR-ANON]>shtek bnusa bo logo:-   "))
+  os.system("figlet "+N29)
+  N30=input("[PROFESSOR-ANON]>shtek bnusa bo logo:-   ")
+  os.system("figlet "+N30)
+  N31=input("[PROFESSOR-ANON]>shtek bnusa bo logo:-   ")
+  os.system("figlet "+N31)
+  N32=input("[PROFESSOR-ANON]>shtek bnusa bo logo:-   ")
+  os.system("figlet "+N32)
+  N33=input("[PROFESSOR-ANON]>shtek bnusa bo logo:-   ")
+  os.system("figlet "+N33)
+  N34=input("[PROFESSOR-ANON]>shtek bnusa bo logo:-   ")
+  os.system("figlet "+N34)
+  N35=input("[PROFESSOR-ANON]>shtek bnusa bo logo:-   ")
+  os.system("figlet "+N35)
+  N36=input("[PROFESSOR-ANON]>shtek bnusa bo logo:-   ")
+  os.system("figlet "+N36)
+  N37=input("[PROFESSOR-ANON]>shtek bnusa bo logo:-   ")
+  os.system("figlet "+N37)
+  N38=input("[PROFESSOR-ANON]>shtek bnusa bo logo:-   ")
+  os.system("figlet "+N38)
+  N39=input("[PROFESSOR-ANON]>shtek bnusa bo logo:-   ")
+  os.system("figlet "+N39)
+  N40=input("[PROFESSOR-ANON]>shtek bnusa bo logo:-   ")
+  os.system("figlet "+N40)
+  N41=input("[PROFESSOR-ANON]>shtek bnusa bo logo:-   ")
+  os.system("figlet "+N41)
+  N42=input("[PROFESSOR-ANON]>shtek bnusa bo logo:-   ")
+  os.system("figlet "+N42)
+  N43=input("[PROFESSOR-ANON]>shtek bnusa bo logo:-   ")
+  os.system("figlet "+N43)
+  exit()
+def anon0():
+  print("""\033[0;32m ____________________________________________
+[KURDISH]
+[1] sabarat ba mn  la 11/12-June/2024 drostkrawm la layan Professor      
+anon darbaray Anon sayre xwarawa bka
+____________________________________________
+[2]>[ANON]>SOCIAL MEDIA
+TELEGRAM  [https://t.me/anonymous_system1]
+TIKTOK    [tiktok.com/@anun0_404]
+____________________________________________
+CREATE BY [PROFESSOR-ANON]
+____________________________________________
+
+[ENGLISH]
+[1] About Me I'm Created in 11/12-june/2024 by Professor Anon About 
+Anon ☟ 
+
+____________________________________________
+[2]>[ANON]>SOCIAL MEDIA
+TELEGRAM  [https://t.me/anonymous_system1]
+TIKTOK    [tiktok.com/@anun0_404]
+____________________________________________
+CREATE BY [PROFESSOR-ANON]
+
+""")
+def anon00():
+  exit()
+if num2 == "1":
+  anon()
+if num2 == "2":
+  anon2()
+  exit()
+if num2 == "00":
+  anon00()
+if num2 == "0":
+  anon0()
+  exit()
